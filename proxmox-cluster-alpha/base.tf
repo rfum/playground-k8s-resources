aa68fed92d58a1424465208f69ec6f307c164933
rfum <fmataraci6@gmail.com>
2024-04-11 19:56:47 +0200

    adding ssh key generation and ssh login configuration

--- proxmox-cluster-alpha/base.tf
+++ proxmox-cluster-alpha/base.tf
@@ -5,3 +5,8 @@ resource "proxmox_virtual_environment_download_file" "debian_cloud_image" {
   file_name    = "debian-12-generic-amd64.qcow2.img"
   url          = "https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.qcow2"
 }
+
+resource "tls_private_key" "debian_vm_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
